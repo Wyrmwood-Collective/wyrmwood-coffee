0d04c1527c2002c2f25aab46a859b963a4ab4781
Ciera Watts <cwatts@catalyte.io>
2026-08-10 11:41:20 -0500

    WC-44 Configure local PostgreSQL database connection

--- src/wyrmwood_coffee/main.py
+++ src/wyrmwood_coffee/main.py
@@ -1,9 +1,20 @@
 import subprocess
+from contextlib import asynccontextmanager
 from pathlib import Path
 
 from fastapi import FastAPI
 
-app = FastAPI()
+from wyrmwood_coffee.database import Base, engine
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    Base.metadata.create_all(bind=engine)
+    yield
+    Base.metadata.drop_all(bind=engine)
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 def dev():
