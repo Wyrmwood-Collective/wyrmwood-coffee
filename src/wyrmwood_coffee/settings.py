928dc0e4be46fe9f6b23b9465d87a6627e8622f9
Bryan Verble <bverble@catalyte.io>
2026-08-10 15:09:47 -0500

    Add fixtures for tests against database

--- src/wyrmwood_coffee/settings.py
+++ src/wyrmwood_coffee/settings.py
@@ -2,7 +2,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    database_url: str
+    database_url: str | None
+    test_database_url: str
 
     model_config = SettingsConfigDict(
         env_file=".env",
