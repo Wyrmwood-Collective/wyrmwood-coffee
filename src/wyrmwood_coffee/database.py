32b7f0641bd691d4b043471e4bdfa45f568357d4
Bryan Verble <bverble@catalyte.io>
2026-08-10 15:09:47 -0500

    Add fixtures for tests against database

--- src/wyrmwood_coffee/database.py
+++ src/wyrmwood_coffee/database.py
@@ -1,7 +1,12 @@
+import sys
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 from wyrmwood_coffee.settings import settings
+
+if settings.database_url is None:
+    sys.exit("Error: DATABASE_URL is not set. Please configure it before running.")
 
 engine = create_engine(settings.database_url)
 
