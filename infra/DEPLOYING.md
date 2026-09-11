087cf5b64d220572989aa9018ae0587247bd6603
Bryan Verble <bverble@catalyte.io>
2026-09-11 14:42:00 -0500

    Update DEPLOYING

---
+++ infra/DEPLOYING.md
@@ -0,0 +1,93 @@
+# Deploying to Azure
+
+## Understanding the Deployment Configuration
+
+This application's deployment infrastructure is managed using [Terraform](https://developer.hashicorp.com/terraform).
+Terraform is a tool for provisioning cloud resources using declarative configuration.
+In other words, instead of manually provisioning cloud resources using a cloud provider's web interface, you write configuration files (`infra/*.tf`) that specify the desired configuration, and then you run `terraform apply` and it makes all the necessary changes for you.
+Managing cloud resources in this way allows managing changes with version control software, just as if it were any other part of your application code (and so people often refer to this concept as "infrastructure as code").
+
+Terraform must maintain a *state file* that describes the current set of provisioned resources and their current configurations.
+This state file needs to be shared among any team member who needs to update the deployment.
+However, the state file contains sensitive information (like database passwords), so it can't be checked into version control.
+One approach, the approach this application takes, is to store the state file in a manually provisioned object storage resource.
+Terraform updates this state file whenever the configuration is updated (by editing the configuration files) and applied (by running `terraform apply`).
+The tool will obtain a lock on the file before making changes in order to prevent multiple users from updating the infrastructure at the same time.
+
+## Installing Dependencies
+
+Install the Azure command-line tool and Terraform:
+
+```PowerShell
+winget install -e --id Microsoft.AzureCLI
+winget install -e --id Hashicorp.Terraform
+```
+
+Login to Azure:
+
+```PowerShell
+az login
+```
+
+## Bootstrapping
+
+The owner of the Azure account will need to perform the one-time bootstrap.
+The `infra/boot.ps1` script creates the resource group and storage account that holds the Terraform state file,
+and creates the Terraform admin group that will have permission to provision resources.
+
+The boot script is idempotent and will add the current user (the Azure account owner) to the Terraform admin group.
+
+## Adding Additional Users to the Admin Group
+
+First, add the user in the Azure portal (Azure Portal → Microsoft Entra ID → Add → User → Invite External User).
+Once the user accepts, this creates a guest object for that user in your Azure directory.
+
+To see current guest users and their object IDs:
+
+```PowerShell
+az ad user list --filter "userType eq 'Guest'" --query "[].{displayName:displayName, upn:userPrincipalName, id:id}" -o json
+```
+
+To see current permissions for a user `$objectId`:
+
+```PowerShell
+az role assignment list --assignee $objectId --all --include-groups -o json
+```
+
+To add the user `$objectId` to the Terraform admin group:
+
+```PowerShell
+az ad group member add --group "terraform-infra-admins" --member-id $objectId
+```
+
+## Applying Configuration Changes
+
+**Note:** Run `terraform` from the `infra` directory.
+You may also run from the project root if you add `chdir=infra` to your invocation.
+
+First initialize Terraform (all users must do this on a new machine):
+
+```PowerShell
+terraform init
+```
+
+To preview changes:
+
+```PowerShell
+terraform plan
+```
+
+To apply changes:
+
+```PowerShell
+terraform apply
+```
+
+## Other Commands
+
+To see the database hostname and admin password:
+
+```PowerShell
+terraform output postgres_fqdn
+terraform output -raw postgres_admin_password
+```
--- infra/boot.ps1
+++ infra/boot.ps1
@@ -1,52 +1,76 @@
+$subscriptionId = "9d3db13e-5092-43f3-94e9-e1e916637239"
+$configResourceGroup = "wyrmwood_collective"
+$location = "centralus"
+$storageAccount = "wyrmwoodstorage"
+$adminGroup = "terraform-infra-admins"
+
 $ErrorActionPreference = "Stop"
 
-$repoRoot = (Resolve-Path (Join-Path $PSScriptRoot "..")).Path
-$currentDir = (Resolve-Path ".").Path
-if ($currentDir -ne $repoRoot) {
-  Write-Host "[boot] This script must be run from the project root."
-  Write-Host "[boot] Project Root = $repoRoot"
-  return
-}
-
 function Invoke-Step {
-  param([string]$Description, [scriptblock]$Command)
-  Write-Host "[boot] $Description"
-  & $Command
-  if ($LASTEXITCODE -ne 0) {
-    throw "[boot] ERROR: non-zero exit code: $LASTEXITCODE"
-  }
+    param([string]$Description, [scriptblock]$Command)
+    Write-Host "[boot] $Description"
+    & $Command
+    if ($LASTEXITCODE -ne 0) {
+        throw "[boot] ERROR: non-zero exit code: $LASTEXITCODE"
+    }
 }
 
 Invoke-Step "Creating resource group for the object storage account..." {
-  az group create --name wyrmwood_collective --location centralus
+    az group create --name $configResourceGroup --location $location
 }
 
 Invoke-Step "Creating storage account for the state container..." {
-  az storage account create `
-    --name wyrmwoodstorage `
-    --resource-group wyrmwood_collective `
-    --sku Standard_LRS `
-    --encryption-services blob
+    az storage account create `
+        --name $storageAccount `
+        --resource-group $configResourceGroup `
+        --sku Standard_LRS `
+        --encryption-services blob
 }
 
 Invoke-Step "Enabling blob versioning on the storage account (for versioning the state file)..." {
-  az storage account blob-service-properties update `
-    --account-name wyrmwoodstorage `
-    --enable-versioning true
+    az storage account blob-service-properties update `
+        --account-name $storageAccount `
+        --enable-versioning true
 }
 
 Invoke-Step "Creating the storage container to hold the Terraform state file..." {
-  az storage container create `
-    --name tfstate `
-    --account-name wyrmwoodstorage
+    az storage container create `
+        --name tfstate `
+        --account-name $storageAccount
 }
 
-Invoke-Step "Initializing Terraform..." {
-  terraform -chdir=infra init
+Invoke-Step "Creating group for Terraform admins..." {
+    az ad group create --display-name $adminGroup --mail-nickname $adminGroup
+}
+
+Invoke-Step "Assigning permissions to Terraform admins group for resource group..." {
+    az role assignment create `
+        --assignee-object-id $(az ad group show --group $adminGroup --query id -o tsv) `
+        --assignee-principal-type Group `
+        --role "Contributor" `
+        --scope "/subscriptions/$subscriptionId/resourceGroups/$configResourceGroup"
+}
+
+Invoke-Step "Assigning permissions to Terraform admins group for storage account..." {
+    az role assignment create `
+        --assignee-object-id $(az ad group show --group $adminGroup --query id -o tsv) `
+        --assignee-principal-type Group `
+        --role "Storage Blob Data Contributor" `
+        --scope "/subscriptions/$subscriptionId/resourceGroups/$configResourceGroup/providers/Microsoft.Storage/storageAccounts/$storageAccount"
+}
+
+Invoke-Step "Adding current user to Terraform admins group..." {
+    $currentUserId = $(az ad signed-in-user show --query id -o tsv)
+    if ($(az ad group member check --group $adminGroup --member-id $currentUserId --query value -o tsv) -ceq "false") {
+        az ad group member add --group $adminGroup --member-id $currentUserId
+    }
 }
 
 Write-Host "[boot] Boot process finished successfully."
-Write-Host '[boot] Run `terraform -chdir=infra apply` to provision.'
+Write-Host '[boot] Run `terraform init` to initialize Terraform,'
+Write-Host '[boot] then `terraform apply` to provision.'
+Write-Host "[boot] You may need to wait for permissions to propagate."
+Write-Host "[boot] If you get an error, wait and try again."
 
 # create the service principal (for CI/CD authentication)
 # save this information somwhere safe!
